2ce08d453b697978cb514a8797a3140a2d0480b0
dtmesa <106643832+dtmesa@users.noreply.github.com>
2024-06-27 02:39:29 -0700

    Update cryptography-lab.c

--- lab-assignment7/cryptography-lab.c
+++ lab-assignment7/cryptography-lab.c
@@ -1,5 +1,5 @@
-//enter your name here
-//enter your email here
+//Dylan Mesa
+//mesa.d@northeastern.com
 
 #include <stdio.h>
 #include <string.h>
@@ -8,15 +8,21 @@
 /* function to encrypt the data*/
 void encrypt(char text[], int key)
 {
-    // Add your code here
+    int len = strlen(text);
+    int startVal = 'a';
+    for(int i = 0; i < len; i++){
+        text[i] = (text[i] + key - startVal) % 26 + startVal;
+    }
 }
 
 /*function to decrypt the data*/
 void decrypt(char text[],int key)
 {
-    
-    // Add your code here
-    
+    int len = strlen(text);
+    int startVal = 'a';
+    for(int i = 0; i < len; i++){
+        text[i] = (text[i] - key - startVal + 26) % 26 + startVal;
+    }
 }
 
 
