3ede3005c3765a74fb3cca33f0583b78c8676f32
dtmesa <106643832+dtmesa@users.noreply.github.com>
2024-06-27 03:36:32 -0700

    Update hw7n.c

--- assignment7/hw7n.c
+++ assignment7/hw7n.c
@@ -1,7 +1,7 @@
-// name: <your name here>
-// email: <your email here>
+// name: Dylan Mesa
+// email: mesa.d@northeastern.edu
 
-// Hash table with doubly linked list for chaning/
+// Hash table with doubly linked list for chaining/
 #include <stdio.h>
 #include <stdlib.h> 
 
@@ -10,23 +10,28 @@ int BUCKET_SIZE = 10;
 
 // node struct
 struct node {
-
-    // Add your code here
-
+    int key;
+    int value;
+    struct node* next;
+    struct node* prev;
 };
 
 // bucket struct
 struct bucket{
-
-    // Add your code here
-
+    struct node* head;
 };
 
 // create a new node
 struct node* createNode(int key, int value){
-
-    // Add your code here
-
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    if(newNode == NULL){
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
+    newNode->key = key;
+    newNode->value = value;
+    newNode->next = NULL;
+    newNode->next = NULL;
     return newNode;
 }
 
@@ -38,26 +43,66 @@ int hashFunction(int key){
 //  insert a new key
 void add(int key, int value){
     int hashIndex = hashFunction(key);
-    
-    // Add your code here
-
+    struct node* newNode = createNode(key, value);
+    //bucket empty
+    if(hashTable[hashIndex].head == NULL){
+        hashTable[hashIndex].head = newNode;
+        printf("Added %d to table with key %d.\n", value, key);
+    }
+    //bucket not empty
+    else{
+        newNode->next = hashTable[hashIndex].head;
+        hashTable[hashIndex].head->prev = newNode;
+        hashTable[hashIndex].head = newNode;
+        printf("Added %d to table with key %d.\n", value, key);
+    }
 }
 
 // remove a key
 void remove_key(int key){
     int hashIndex = hashFunction(key);
-
-    // Add your code here
-
+    struct node* curr = hashTable[hashIndex].head;
+    while(curr != NULL){
+        //head is key
+        if(curr->key == key && curr->prev == NULL){
+            //more to list
+            if(curr->next != NULL){
+                curr->next->prev = NULL;
+                hashTable[hashIndex].head = curr->next;
+                printf("Removed key %d from table.\n", key);
+                return;
+            }
+            //list empty
+            else{
+                hashTable[hashIndex].head = NULL;
+                printf("Removed key %d from table.\n", key);
+                return;
+            }
+        }
+        //key at middle or end of list
+        else if(curr->key == key && curr->prev != NULL){
+            curr->prev->next = curr->next;
+            printf("Removed key %d from table.\n", key);
+            return;
+        }
+        //go next node
+        curr = curr->next;
+    }
+    printf("Key not found.\n");
 }
 
 // search a value using a key
 void search(int key){
     int hashIndex = hashFunction(key);
     struct node* node = hashTable[hashIndex].head;
-
-    // Add your code here
-
+    while(node != NULL){
+        if(node->key == key){
+            printf("Key %d found with value %d.\n", node->key, node->value);
+            return;
+        }
+        node = node->next;
+    }
+    printf("Key not found.\n");
 }
 
 void display(){
