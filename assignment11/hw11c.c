1d299b7d79ab6679530d07e13423b8c395a0e318
mesa.d <mesa.d@northeastern.edu>
2024-07-21 21:54:14 -0700

    hw

--- assignment11/hw11c.c
+++ assignment11/hw11c.c
@@ -1,14 +1,28 @@
-// name: <your name here>
-// email: <your email here>
+// name: Dylan Mesa
+// email: mesa.d@northeastern.edu
 
 #include <stdio.h>
 
 int d[20];
 
 long long int dp(int n) {
+    if(n == 0){
+        return 1;
+    }
+    
+    d[0] = 1;
+    d[1] = 0;
+    d[2] = 3;
+    
+    for(int i = 3; i <= n; i++){
+        d[i] = 3 * d[i - 2];
 
-    // Add your code here
-
+        for(int j = 4; j <= i; j += 2){
+            d[n] += 2 * d[i - j];
+        }
+    }
+    
+    return d[n];
 }
 
 int main(void) {
