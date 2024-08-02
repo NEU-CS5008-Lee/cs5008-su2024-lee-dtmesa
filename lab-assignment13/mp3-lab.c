0c9a073ce32c650bbe48ba3a3cff9398b7affcda
dtmesa <dylantmesa@gmail.com>
2024-08-02 10:35:45 -0700

    lab

--- lab-assignment13/mp3-lab.c
+++ lab-assignment13/mp3-lab.c
@@ -1,20 +1,41 @@
-//enter your email here
-//enter your name here
+//mesa.d@northeastern.edu
+//Dylan Mesa
 
 #include<stdio.h>
 #include<string.h>
 
 int compression(char arr[], int n, char res[]){
-    
-    
-    
-    
-    //insert your code here
-    
-    
-    
-    
-    
+    char ch;
+    int count = 1;
+    int index = 0;
+    int size = 0;
+    if(n == 0){
+        return 0;
+    }
+    for(int i = 0; i < n; i++){
+        ch = arr[i];
+        if(i < (n - 1) && arr[i] == arr[i + 1]){
+            count++;
+        } else {
+            if(count != 1){
+                res[index] = ch;
+                res[index + 1] = count;
+                index += 2;
+                size += 2;
+                int temp = count;
+                while(temp / 10 > 0){
+                    size++;
+                    temp /= 10;
+                }
+            } else {
+                res[index] = ch;
+                index++;
+                size += 1;
+            }
+            count = 1;
+        }
+    }
+    return size;
 }
  
 int main()
